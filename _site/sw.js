e3de457559ecf5ef1ee34840f66beafdcea6bcb7
Yoon-Min <yoonmin0113@naver.com>
2023-01-06 13:32:39 +0900

    블로그 세팅 마무리 작업
    - favicon 이미지 변경
    - 프로필 이미지 변경
    - 네이버 검색 엔진 등록에 필요했던 html 삭제
    - 테스트용 임시 포스트 추가

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230105.224011';
+const cacheName = 'chirpy-20230106.133158';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
