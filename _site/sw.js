f2625fcfcbcbded31093d414cd45c854f60d60d9
Yoon-Min <yoonmin0113@naver.com>
2023-01-12 00:50:40 +0900

    깃허브 블로그 만들기 글 작성(일부)
    시작: 시작하기에 앞서 참고해주세요!
    끝: 테마 파일들 가져오기 - 그런데 문제가....

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230106.133158';
+const cacheName = 'chirpy-20230112.004949';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
