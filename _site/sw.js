b6e1e0b798e3bf65512510a36faef291065ecbf4
Yoon-Min <yoonmin0113@naver.com>
2023-02-09 23:43:30 +0900

    깃허브 블로그 글 일부 작성
    - 안드로이드 생명 주기_1

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230114.143957';
+const cacheName = 'chirpy-20230209.193615';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
