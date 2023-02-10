9d1cfd51189d6f8386bc38b3d1694c9a70f94c4f
Yoon-Min <yoonmin0113@naver.com>
2023-02-11 01:00:15 +0900

    [포스팅]: 안드로이드 생명주기
    - 생명주기 사진 첨부

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230210.233455';
+const cacheName = 'chirpy-20230211.005943';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
