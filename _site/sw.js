67410ab8f87ccd5ef9e553d9d32c391cb3e84cb1
Yoon-Min <yoonmin0113@naver.com>
2023-02-20 00:19:17 +0900

    [포스팅 : 진행] 유니온 파인드
    구현과 관리를 위해 사용되는 방법 ~ 연결 리스트를 통한 관리 끝까지

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230217.164346';
+const cacheName = 'chirpy-20230220.001910';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
