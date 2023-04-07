f31ac9fd2f65109b2cb341744d6cc5d0959ee1ea
Yoon-Min <yoonmin0113@naver.com>
2023-04-08 01:20:43 +0900

    [포스팅:수정] Android의 UI, 뷰(View) 그리고 레이아웃(ViewGroup)
    - 일부 소제목 텍스트 굵은 효과 제거

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230408.011905';
+const cacheName = 'chirpy-20230408.012030';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
