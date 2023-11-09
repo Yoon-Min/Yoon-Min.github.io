64c0c0828c81dbf83f0c11ea7670bcad01396e25
Yoon-Min <yoonmin0113@naver.com>
2023-11-09 15:25:39 +0900

    [포스팅:진행] Kotlin Generic 제네릭7

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231108.175456';
+const cacheName = 'chirpy-20231109.152509';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
