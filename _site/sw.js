a714f3c98b26a62f9916f3e3b56c52ccb462fb49
Yoon-Min <yoonmin0113@naver.com>
2024-02-02 02:56:31 +0900

    [포스팅:진행] Goodbye, 2023! Welcome, 2024! - 8

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240201.225312';
+const cacheName = 'chirpy-20240202.025619';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
