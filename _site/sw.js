9986947caaaf61bea1cc1c618cc18114eb3361e4
Yoon-Min <yoonmin0113@naver.com>
2024-02-22 19:57:36 +0900

    [문서:수정] 네이버 사이트 소유 인증 메타 태그 추가

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240222.194343';
+const cacheName = 'chirpy-20240222.195337';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
