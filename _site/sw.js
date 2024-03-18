b75d950b70cdf7fcee8a078dca6d5d2eabc854e3
Yoon-Min <yoonmin0113@naver.com>
2024-03-19 00:43:27 +0900

    [포스팅:진행] Kotlin - 고차 함수와 inline 2편 - 2

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240318.234418';
+const cacheName = 'chirpy-20240319.004303';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
