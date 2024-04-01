516295214d70a783c4dc2dec1efafa15753f64cd
Yoon-Min <yoonmin0113@naver.com>
2024-04-01 13:48:01 +0900

    [문서:수정] 일부 포스팅 파일 제목 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240401.134530';
+const cacheName = 'chirpy-20240401.134740';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
