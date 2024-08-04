4106e7a4e9833b1ff909d09cd73a729bebd84702
Yoon-Min <yoonmin0113@naver.com>
2024-08-04 23:40:31 +0900

    블로그 디자인 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240803.192937';
+const cacheName = 'chirpy-20240804.233511';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
