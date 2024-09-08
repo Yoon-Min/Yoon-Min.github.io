8c5b20615a55db98ee856e41429d686597effc3e
Yoon-Min <yoonmin0113@naver.com>
2024-09-08 23:54:02 +0900

    일부 포스트 고정 해제

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240901.163148';
+const cacheName = 'chirpy-20240908.235336';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
