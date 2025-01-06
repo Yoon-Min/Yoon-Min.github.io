b79dcf8476fa8ad28e1de72247c428546aed27cf
Yoon-Min <yoonmin0113@naver.com>
2025-01-06 22:35:18 +0900

    [포스팅:진행] 2024 리뷰 일부 작성`

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250104.160750';
+const cacheName = 'chirpy-20250105.164553';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
