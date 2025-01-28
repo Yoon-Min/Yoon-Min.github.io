3718f994b62dfb1da4124da99c0e1b069c493f99
Yoon-Min <yoonmin0113@naver.com>
2025-01-28 09:27:11 +0900

    [수정] 일부 문서 제목명 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250121.131008';
+const cacheName = 'chirpy-20250128.092620';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
