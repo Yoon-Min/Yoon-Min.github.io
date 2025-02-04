459b71103144dca433ecf088343ecc3a2f9022da
Yoon-Min <yoonmin0113@naver.com>
2025-02-04 23:07:03 +0900

    [수정] 워크플로 일부 버전 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250204.224955';
+const cacheName = 'chirpy-20250204.230650';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
