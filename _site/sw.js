7024cf29114f49ad3f606efda8d89afaf8f9b954
Yoon-Min <yoonmin0113@naver.com>
2025-02-04 22:44:23 +0900

    [추가] 누락 파일 추가

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250204.224124';
+const cacheName = 'chirpy-20250204.224407';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
