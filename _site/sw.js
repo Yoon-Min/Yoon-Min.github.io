daea3e6045c9cd258ee382620ae6c330d88e3223
Yoon-Min <yoonmin0113@naver.com>
2025-02-04 22:50:19 +0900

    [추가] 일부 글 제거

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250204.224407';
+const cacheName = 'chirpy-20250204.224955';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
