fe8ff867531e861e73f0b017951cdf072c735277
Yoon-Min <yoonmin0113@naver.com>
2025-03-01 22:20:15 +0900

    [포스팅:수정] 네비게이션 관련 일부 글 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250301.221407';
+const cacheName = 'chirpy-20250301.221956';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
