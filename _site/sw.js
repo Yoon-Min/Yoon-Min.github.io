405bcc65b2ea205c389ddb608c27a36aae78d64d
Yoon-Min <yoonmin0113@naver.com>
2025-03-01 22:16:31 +0900

    [포스팅:작성] Snapshot 시스템 글 일부 작성

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250225.235209';
+const cacheName = 'chirpy-20250301.221407';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
