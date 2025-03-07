c96c39565d27377424fadc14fd34a80296908b1c
윤승민 <yunseungmin@yunseungmin-ui-MacBookPro.local>
2025-03-07 16:24:46 +0900

    [문서:초기화] 마이그레이션을 위한 초기화

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250303.154028';
+const cacheName = 'chirpy-20250307.162331';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
