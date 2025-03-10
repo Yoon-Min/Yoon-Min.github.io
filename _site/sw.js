f793b6fb978b08e06a4c3d066c99b7895ee93a37
Yoon-Min <yoonmin0113@naver.com>
2025-03-10 22:23:42 +0900

    [문서:수정] Link hover 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250310.212735';
+const cacheName = 'chirpy-20250310.220643';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
