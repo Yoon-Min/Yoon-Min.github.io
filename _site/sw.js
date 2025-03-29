a566aa29599df1854d9c0e52981b6f484f3241ec
Yoon-Min <yoonmin0113@naver.com>
2025-03-29 19:49:33 +0900

    meta: 빌드 파일 추가

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250329.194803';
+const cacheName = 'chirpy-20250329.194907';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
