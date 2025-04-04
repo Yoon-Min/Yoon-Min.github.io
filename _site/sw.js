fb95a2b2f41312067a5d88335dc21e8f85b0a417
Yoon-Min <yoonmin0113@naver.com>
2025-04-04 13:36:20 +0900

    asset: 프로필 사진 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250404.133215';
+const cacheName = 'chirpy-20250404.133529';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
